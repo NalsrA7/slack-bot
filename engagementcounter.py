360ed252c7088cf955b2b4372de1cd0137fe5990
Arslan Ahmad <arslanshero@gmail.com>
2022-05-30 14:49:52 +0700

    now outputs msg text also

--- engagementcounter.py
+++ engagementcounter.py
@@ -14,6 +14,7 @@ def engagement_counter():
     timed_messages = []
     emojis = {}
     replies = {}
+    msg_text = {}
 
         # Load the message history from the JSON file
     with open('conversations.json', 'r') as f:
@@ -45,16 +46,21 @@ def engagement_counter():
             replies[timestamp] = message["reply_users_count"]
         except:
             pass
+        
+        try:    
+            msg_text[timestamp] = str(message["text"])
+        except:
+            pass
                 
     # Currently if a message has reactions but no replies, our script still returns it but shows replies as 0
     # However, if a message has replies but no reactions, the script will not return any result for this and skip it
     # Because most (if not all) messages that have replies have reactions, this is not a problem
-    header = ['timestamp', 'emoji_count', 'reply_count']
-    with open('engagement_count.csv', 'w', newline='') as csvfile:
+    header = ['timestamp', 'emoji_count', 'reply_count', 'message_text']
+    with open('engagement_count.csv', 'w', newline='', encoding="utf-8") as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(header)
         for key, value in emojis.items():
-            writer.writerow([key, str(value), (str(replies[key]) if key in replies else "0")])
+            writer.writerow([key, str(value), (str(replies[key]) if key in replies else "0"), (msg_text[key] if key in replies else "")])
             
     print("Successfully exported engagement count to engagement_count.csv")
     print("Total Number Of Messages with Reactions:", len(emojis))
